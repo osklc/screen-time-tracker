05b9c5a6fb0034a64afd915c24f7cb0cc8ead94e
osklc <osklcmail@gmail.com>
2026-08-11 19:47:58 +0300

    feat: add tauri-plugin-single-instance for managing single app instances

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1510,6 +1510,13 @@ pub fn run() {
         .plugin(tauri_plugin_autostart::init(tauri_plugin_autostart::MacosLauncher::LaunchAgent, None))
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.show();
+                let _ = window.unminimize();
+                let _ = window.set_focus();
+            }
+        }))
         .setup(|app| {
             let app_handle = app.handle().clone();
             let power_state = app.state::<PowerMonitorState>().inner().clone();
